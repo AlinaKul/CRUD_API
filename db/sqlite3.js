526d981fe791bd0af1ac010c619e55942b384876
unknown <alinavilcin@gmail.com>
2021-03-21 10:36:29 +0200

    Update sqlite3.js

--- db/sqlite3.js
+++ db/sqlite3.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database(':memory:')
 
-/*db.run('CREATE TABLE trains( id integer NOT NULL, model text NOT NULL, yearOfManufacturing numeric NOT NULL, lastMaintenanceDate numeric NOT NULL, isActive integer);', function(err){
+/*db.run('CREATE TABLE trains( id integer NOT NULL, model text NOT NULL, yearOfManufacturing text NOT NULL, lastMaintenanceDate text NOT NULL, isActive integer);', function(err){
   if (err){
     return console.log(err.message)
   }
@@ -11,15 +11,15 @@ let db = new sqlite3.Database(':memory:')
 db.close()*/
 
 db.serialize(function() {
-  db.run("CREATE TABLE trains2 (id INTEGER PRIMARY KEY, model text NOT NULL, yearOfManufacturing text NOT NULL, lastMaintenanceDate text NOT NULL, isActive integer)");
+  db.run("CREATE TABLE trains (id INTEGER PRIMARY KEY, model text NOT NULL, yearOfManufacturing text NOT NULL, lastMaintenanceDate text NOT NULL, isActive integer)");
 
-  var stmt = db.prepare("INSERT INTO trains2 VALUES (id, model, yearOfManufacturing, lastMaintenanceDate, isActive)");
+  var stmt = db.prepare("INSERT INTO trains VALUES (id, model, yearOfManufacturing, lastMaintenanceDate, isActive)");
   for (var i = 0; i < 10; i++) {
       stmt.run("trains " + i);
   }
   stmt.finalize();
 
-  db.each("SELECT rowid AS id, info FROM trains2", function(err, row) {
+  db.each("SELECT rowid AS id, info FROM trains", function(err, row) {
       console.log(row.id + ": " + row.info);
   });
 });
